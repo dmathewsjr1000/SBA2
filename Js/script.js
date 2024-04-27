9d7c52a8af8a501a7564f68a586b3cb45e684f0e
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-27 18:29:34 -0400

    Files Updated

---
+++ Js/script.js
@@ -0,0 +1,71 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password1 = document.getElementById('password1');
+
+form.addEventListener('submit', e => {    
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const regex = /^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/;
+    return regex.test(String(email).toLocaleLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue =username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password1Value = password1.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required:');
+    }else{
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required: ');
+    }else if (!isValidEmail(emailValue)){
+        setError(email, 'Provide a valid email address:')
+    }else {
+        setSuccess(email);
+    }
+    
+    if (password1Value === '') {
+        setError(password, 'Password is required:');
+    }else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 characters.');
+    }else {
+        setSuccess(password);
+    }
+
+    if (password1Value === '') {
+        setError(password1, 'Please confirm your password.')
+    } else if(password1 !== passwordValue) {
+        setError(password1, "Password doesn't match.");
+    }else {
+        setSuccess(password1);
+    }
+};
