6dbd814b1fb97d7fe8fad72097f7a133ab0a79f5
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-30 00:08:28 -0400

    File Updated

--- Js/script.js
+++ Js/script.js
@@ -4,13 +4,30 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password1 = document.getElementById('password1');
+// Declaring variables and changing Attributes
+const topMenuEl = document.querySelector("nav");
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.style.height = "100%";
+topMenuEl.className = "flex-around";
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+// Looping though the array creating a elements,setting attributes to href and adding text plus apprend child element
+for (let i = 0; i < menuLinks.length; i++) {
+    const a = document.createElement("a");
+    a.setAttribute("href", menuLinks[i].href);
+    a.textContent = menuLinks[i].text;
+    topMenuEl.appendChild(a);
+  }
 //  Adding a event listerner on the submit button
 form.addEventListener('submit', e => {    
     e.preventDefault();
-
     validateInputs();
 });
-// Declaring function
+// Declaring error function
 const setError = (element, message) => {
     //  Declaring Variables of function
     const inputControl = element.parentElement;
@@ -20,7 +37,7 @@ const setError = (element, message) => {
     inputControl.classList.add('error');
     inputControl.classList.remove('success');
 }
-// Declaring function
+// Declaring success function
 const setSuccess = element => {
     //  Declaring Variables of function
     const inputControl = element.parentElement;
@@ -30,12 +47,12 @@ const setSuccess = element => {
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
-// Declaring function
+// Declaring email function
 const isValidEmail = email => {
-    const regex = /^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/;
+    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
     return regex.test(String(email).toLocaleLowerCase());
 }
-// Declaring function
+// Declaring validate function
 const validateInputs = () => {
 //  Declaring Variables of function 
     const usernameValue =username.value.trim();
@@ -67,8 +84,8 @@ const validateInputs = () => {
 // Testing the password 2 if match and if password was put in
     if (password1Value === '') {
         setError(password1, 'Please confirm your password.')
-    } else if(password1 !== passwordValue) {
-        setError(password1, "Password doesn't match.");
+    } else if(password1Value !== passwordValue) {
+        setError(password1Value, "Password doesn't match.");
     }else {
         setSuccess(password1);
     }
